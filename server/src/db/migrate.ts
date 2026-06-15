bfafb1890e6b38cbf7744fc0504404fce145eeae
Diltechng <diltechng@gmail.com>
2026-06-15 11:13:48 +0100

    migration

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -2,31 +2,45 @@ import "dotenv/config";
 import { pool } from "./pool";
 
 const migrate = async () => {
-  console.log("🔥 MIGRATION FILE RUNNING");
   const client = await pool.connect();
   console.log(client);
   try {
-    await client.query(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`);
-    await client.query("BEGIN");
+    console.log("MIGRATION FILE RUNNING");
 
+    await client.query(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`);
+    console.log("pgcrypto");
+
+    await client.query("BEGIN");
+    console.log("BEGIN");
+
+    await client.query(`DROP TABLE IF EXISTS users CASCADE`);
+    await client.query(`DROP TABLE IF EXISTS stock CASCADE`);
+
+    //users
     await client.query(`
       CREATE TABLE IF NOT EXISTS users (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255)
+      )
+    `);
+    console.log("users created");
+
+    //stock
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS stock (
+        id   BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        public_id UUID NOT NULL DEFAULT gen_random_uuid() UNIQUE,
         name VARCHAR(255) NOT NULL
       )
     `);
+    console.log("stocks created");
 
     await client.query("COMMIT");
-    console.log("✅ Migration completed");
+    console.log("Migration completed");
   } catch (err) {
     await client.query("ROLLBACK");
-
-    if (err instanceof Error) {
-      console.error("❌ Migration failed:", err.message);
-    } else {
-      console.error("❌ Migration failed:", err);
-    }
-
+    console.error("Migration failed:", err);
     throw err;
   } finally {
     client.release();
@@ -34,4 +48,7 @@ const migrate = async () => {
   }
 };
 
-migrate().catch(() => process.exit(1));
+migrate().catch((err) => {
+  console.error("TOP LEVEL ERROR:", err);
+  process.exit(1);
+});
