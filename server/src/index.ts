dc84de8445fb96d1731738b962e7d521dcc6d06c
Muhammad Muslih Muhammad <muslih.baba22@gmail.com>
2026-09-17 11:50:20 +0100

    deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ const PORT = Number(process.env.PORT) || 4000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
