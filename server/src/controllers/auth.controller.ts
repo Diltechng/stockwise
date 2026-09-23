51900f29053230091e8bd3d8d6cf40e70a95e984
Muhammad Muslih Muhammad <muslih.baba22@gmail.com>
2026-09-23 17:00:44 +0100

    fix: change auth cookie samesite strategy to none

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -152,7 +152,7 @@ export const login = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
